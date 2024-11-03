12fd25800c907d54fa09b5405df524f96254f4a4
Jan Ostrizki <ostrjan@outlook.de>
2024-11-03 19:19:21 +0100

    Added solution

--- src/adap-b02/names/StringArrayName.ts
+++ src/adap-b02/names/StringArrayName.ts
@@ -6,51 +6,75 @@ export class StringArrayName implements Name {
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+        this.components = other;
+        if(delimiter != null)
+        {
+            this.delimiter = delimiter;
+        }
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        return this.components.map(s => s.replaceAll(ESCAPE_CHARACTER+delimiter, delimiter)).join(delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation");
+        return this.components.join(this.delimiter);
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation");
+        return this.components.length == 0;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation");
+        return this.delimiter;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.components.length;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation");
+        if(i >= this.components.length || i < 0)
+        {
+            throw new Error("Out of Bounce");
+        }
+        return this.components[i];
     }
 
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+        if(i >= this.components.length || i < 0)
+            {
+                throw new Error("Out of Bounce");
+            }
+
+        this.components[i] = c;
     }
 
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+        if(i < this.components.length && i >= 0)
+        {
+            this.components = this.components.slice(0,i).concat(c).concat(this.components.slice(i));
+        }
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.components = this.components.concat(c);
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation");
+        if(i >= this.components.length || i < 0)
+        {
+            throw new Error("Out of Bounce");
+        }
+        this.components=this.components.slice(0,i).concat(this.components.slice(i+1));
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        // Adding another String Array to another
+        for(let i = 0; i < other.getNoComponents(); i++)
+        {
+            this.append(other.getComponent(i))
+        }
     }
 
 }
--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -8,51 +8,219 @@ export class StringName implements Name {
     protected length: number = 0;
 
     constructor(other: string, delimiter?: string) {
-        throw new Error("needs implementation");
+        this.name = other;
+        if(delimiter != undefined)
+        {
+            this.delimiter = delimiter;
+        }
+        this.length = this.getNoComponents();
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        return this.name.replaceAll(`${ESCAPE_CHARACTER}${delimiter}`,delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation");
+        return this.name;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation");
+        return this.name === "";
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation");
+        return this.delimiter;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        let stinglen = this.name.length;
+        let i = 0;
+        let count = 1;
+        while(i < stinglen)
+        {
+            let curChar = this.name.charAt(i)
+            if(curChar === ESCAPE_CHARACTER)
+            {
+                if(this.name.charAt(i+1) === this.delimiter)
+                {
+                    i += 2;
+                    continue;
+                }
+            } 
+            if (curChar === this.delimiter)
+            {
+                count++;
+            }
+            i++;
+        }
+        return count;
     }
 
     public getComponent(x: number): string {
-        throw new Error("needs implementation");
+        let stinglen = this.name.length;
+        let i = 0;
+        let count = 0;
+        let record = "";
+        while(i < stinglen)
+        {
+            let curChar = this.name.charAt(i)
+            if(count === x)
+            {
+                if(curChar != this.delimiter)
+                {
+                record += curChar;
+                }else
+                {
+                    return record;
+                }
+            }
+            if(curChar === ESCAPE_CHARACTER)
+            {
+                let nextChar = this.name.charAt(i+1);
+                if(nextChar === this.delimiter)
+                {
+                    record += nextChar;
+                    i += 2;
+                    continue;
+                }
+            }
+            if(curChar === this.delimiter)
+            {
+                count++;
+            }
+            i++;
+        }
+        return record;
     }
 
     public setComponent(n: number, c: string): void {
-        throw new Error("needs implementation");
+        if(n >= this.getNoComponents())
+        {
+            throw new Error("?????????????????");
+        }
+
+
+        let [startPos,endPos] = this.getCompInPos(n)
+        console.log(startPos,endPos)
+
+        if(n === this.getNoComponents()-1)
+        {
+            this.name = this.name.slice(0,startPos).concat(c);
+        } 
+        else if (n < this.getNoComponents()-1) 
+        {
+            this.name = this.name.slice(0,startPos).concat(c).concat(this.name.slice(endPos))
+        }
+    }
+
+    private getCompInPos(n:number) : [number, number]
+    {
+        let count = 0;
+        let i = 0;
+        let startPos = 0;
+        let endPos;
+
+        while (i < this.name.length)
+        {
+            let curChar = this.name.charAt(i);
+            if(curChar === ESCAPE_CHARACTER)
+            {
+                if(this.name.charAt(i+1) === this.delimiter)
+                {
+                    i +=2;
+                    continue;
+                }
+            }
+
+            if(curChar === this.delimiter)
+            {
+                count++;
+                if(count === n)
+                {
+                    startPos = i+1;
+                }
+
+                if(count === n+1)
+                {
+                    endPos = i;
+                    return [startPos,endPos];
+                }
+            }
+            i++;
+        }
+        if(endPos === undefined)
+        {
+            endPos = this.name.length
+        }
+        return [startPos,endPos];
     }
 
     public insert(n: number, c: string): void {
-        throw new Error("needs implementation");
+        if(this.name === "")
+        {
+            console.log(this.getNoComponents())
+        }
+       if(n === this.getNoComponents())
+        {
+           this.append(c);
+        }
+        else 
+        {
+            //normal case
+            let [startPos, endPos] = this.getCompInPos(n);
+           
+            console.log(startPos,endPos)
+            this.name = this.name.slice(0,startPos).concat(c).concat(this.delimiter).concat(this.name.slice(startPos,endPos)).concat(this.name.slice(endPos))
+        }
+        this.length++;
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        if(this.getNoComponents() == 0)
+        {
+            this.name = c;
+        } 
+        else if (this.getComponent(0)==="")
+        {
+            this.name = this.delimiter+c;
+        }
+        else 
+        {
+            this.name = this.name.concat(this.delimiter).concat(c);
+        }
+        this.length++;
     }
 
     public remove(n: number): void {
-        throw new Error("needs implementation");
+        let [startPos,endPos] = this.getCompInPos(n);
+        if(this.getNoComponents() == 1)
+        {
+            this.name = ''
+        } 
+        
+        else if(this.getNoComponents() == n) 
+        {
+            this.name = this.name.slice(0,startPos-1)
+            this.length--;
+        }
+
+        else if(n==0)
+        {
+            this.name = this.name.slice(endPos+1)
+            this.length--;
+        } 
+
+        else
+        {
+            this.name = this.name.slice(0,startPos-1).concat(this.name.slice(endPos))
+            this.length--;
+        }
+
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        this.name.concat(this.delimiter).concat(other.asDataString());
+        this.length = this.getNoComponents();
     }
 
 }
